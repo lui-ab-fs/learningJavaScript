9a86275e9fcdac4644fa005d8f1fb91c7f1eb3f4
Luis Fanovich <35769672+lui-ab-fs@users.noreply.github.com>
2024-12-26 14:37:12 -0500

    Add files via upload

---
+++ Dia 4/7 - Proyecto del dia 4/misScripts.js
@@ -0,0 +1,7 @@
+function mostrarResultado(){
+    let elementoValor1 = document.getElementById("valor1");
+    let elementoValor2 = document.getElementById("valor2");
+    let elementoValorResultado = document.getElementById("valorResultado");
+}
+
+
