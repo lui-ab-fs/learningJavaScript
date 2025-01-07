3b9780a575f8c13c6451205bc4a524ff3f73f8ac
Luis Fanovich <35769672+lui-ab-fs@users.noreply.github.com>
2025-01-07 15:56:37 -0500

    Add files via upload

--- Dia 4/7 - Proyecto del dia 4/misScripts.js
+++ Dia 4/7 - Proyecto del dia 4/misScripts.js
@@ -1,7 +1,78 @@
-function mostrarResultado(){
-    let elementoValor1 = document.getElementById("valor1");
-    let elementoValor2 = document.getElementById("valor2");
-    let elementoValorResultado = document.getElementById("valorResultado");
+function mostrarResultado(resultado){
+    document.getElementById("resultado").value = resultado;
 }
 
+function sumar(){
+    let numero1 = +document.getElementById("primerNumero").value;
+    let numero2 = +document.getElementById("segundoNumero").value;
+    
+    mostrarResultado(numero1 + numero2)
+}
 
+function restar(){
+    let numero1 = +document.getElementById("primerNumero").value;
+    let numero2 = +document.getElementById("segundoNumero").value;
+
+    mostrarResultado(numero1 - numero2)
+}
+
+function multiplicar(){
+    let numero1 = +document.getElementById("primerNumero").value;
+    let numero2 = +document.getElementById("segundoNumero").value;
+
+    mostrarResultado(numero1 * numero2);
+}
+
+function dividir(){
+    let numero1 = +document.getElementById("primerNumero").value;
+    let numero2 = +document.getElementById("segundoNumero").value;
+
+    mostrarResultado(numero1 / numero2);
+}
+
+function raizCuadrada(){
+    let numero1 = +document.getElementById("primerNumero").value;
+    let numero2 = +document.getElementById("segundoNumero").value;
+
+    mostrarResultado(Math.sqrt(numero1));
+}
+
+function potencia(){
+    let numero1 = +document.getElementById("primerNumero").value;
+    let numero2 = +document.getElementById("segundoNumero").value;
+
+    mostrarResultado(Math.pow(numero1, numero2));
+}
+
+function valorAbsoluto(){
+    let numero1 = +document.getElementById("primerNumero").value;
+    let numero2 = +document.getElementById("segundoNumero").value;
+
+    mostrarResultado(Math.abs(numero1));
+}
+
+function random(){
+    let minimo = +document.getElementById("primerNumero").value;
+    let maximo = +document.getElementById("segundoNumero").value;
+
+    maximo = maximo + 1;
+    mostrarResultado(Math.floor(Math.random() * (maximo - minimo) + minimo));
+}
+
+function redondear(){
+    let resultado = +document.getElementById("resultado").value;
+
+    mostrarResultado(Math.round(resultado));
+}
+
+function redondearFloor(){
+    let resultado = +document.getElementById("resultado").value;
+
+    mostrarResultado(Math.floor(resultado));
+}
+
+function redondearCeil(){
+    let resultado = +document.getElementById("resultado").value;
+
+    mostrarResultado(Math.ceil(resultado));
+}
